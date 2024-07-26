aedf00703b2cb56abcf4934df2c4d6d2dfa59e02
Trần Thanh Hải <96519556+haiThanhTran@users.noreply.github.com>
2024-07-27 00:53:33 +0700

    update quantityChange

--- library_management/src/pages/publisher/ManagePublisher.jsx
+++ library_management/src/pages/publisher/ManagePublisher.jsx
@@ -44,19 +44,20 @@ const ManagementPublisher = () => {
       const response = await axios.get("http://localhost:9191/api/publishers");
       setPublishers(response.data);
     } catch (error) {
-      console.error("Error fetching publishers:", error);
+      console.error("Lỗi khi tải nhà xuất bản:", error);
     }
   };
 
   const isPublisherNameValid = (name) => {
-    const regex = /^[a-zA-Z0-9\s]*$/;
+    const regex = /^[a-zA-Z0-9\s\-\.,]*$/;
     return name.trim() !== "" && regex.test(name);
   };
+  
 
   const createPublisher = async () => {
     if (!isPublisherNameValid(newPublisher)) {
       toast.error(
-        "Invalid publisher name. Only letters, numbers, and spaces are allowed."
+        "Tên nhà xuất bản không hợp lệ. Chỉ cho phép chữ cái, số và khoảng trắng."
       );
       return;
     }
@@ -74,20 +75,20 @@ const ManagementPublisher = () => {
       setPublishers([...publishers, response.data]);
       setNewPublisher("");
       setOpenAdd(false);
-      toast.success("Publisher added successfully!");
+      toast.success("Thêm nhà xuất bản thành công!");
     } catch (error) {
-      console.error("Error creating publisher:", error);
-      toast.error("Failed to add publisher.");
+      console.error("Lỗi khi thêm nhà xuất bản:", error);
+      toast.error("Thêm nhà xuất bản thất bại.");
     }
   };
 
   const updatePublisher = async (id, updatedName) => {
-    if (!isPublisherNameValid(updatedName)) {
-      toast.error(
-        "Invalid publisher name. Only letters, numbers, and spaces are allowed."
-      );
-      return;
-    }
+    // if (!isPublisherNameValid(updatedName)) {
+    //   toast.error(
+    //     "Tên nhà xuất bản không hợp lệ. Chỉ cho phép chữ cái, số và khoảng trắng."
+    //   );
+    //   return;
+    // }
 
     try {
       const response = await axios.put(
@@ -104,14 +105,37 @@ const ManagementPublisher = () => {
       );
       setSelectedPublisher(null);
       setOpenEdit(false);
-      toast.success("Publisher updated successfully!");
+      toast.success("Cập nhật nhà xuất bản thành công!");
     } catch (error) {
-      console.error("Error updating publisher:", error);
-      toast.error("Failed to update publisher.");
+      console.error("Lỗi khi cập nhật nhà xuất bản:", error);
+      toast.error("Cập nhật nhà xuất bản thất bại.");
     }
   };
 
   const deletePublisher = async (id) => {
+    // Fetch all books
+    let books;
+    try {
+      const response = await axios.get("http://localhost:9191/api/books");
+      books = response.data;
+    } catch (error) {
+      console.error("Lỗi khi tải sách:", error);
+      toast.error("Tải sách thất bại.");
+      return;
+    }
+
+    // Check if there are any books with the publisher ID to be deleted
+    const booksInPublisher = books.some(
+      (book) => book.publisher.publisherID === id
+    );
+
+    if (booksInPublisher) {
+      toast.error(
+        "Không thể xóa nhà xuất bản này vì có sách liên quan đến nhà xuất bản này !"
+      );
+      return;
+    }
+
     const confirmDelete = window.confirm(
       "Bạn có chắc chắn xóa không? Nếu xóa nhà xuất bản này sẽ xóa các sách liên quan đến nhà xuất bản này"
     );
@@ -125,16 +149,16 @@ const ManagementPublisher = () => {
       });
       setPublishers(publishers.filter((pub) => pub.publisherID !== id));
       setSelectedPublisher(null);
-      toast.success("Publisher deleted successfully!");
+      toast.success("Xóa nhà xuất bản thành công!");
     } catch (error) {
-      console.error("Error deleting publisher:", error);
-      toast.error("Failed to delete publisher.");
+      console.error("Lỗi khi xóa nhà xuất bản:", error);
+      toast.error("Xóa nhà xuất bản thất bại.");
     }
   };
 
   const handleCopyToClipboard = (text) => {
     navigator.clipboard.writeText(text);
-    toast.info("Copied to clipboard!");
+    toast.info("Đã sao chép vào clipboard!");
   };
 
   const filteredPublishers = publishers.filter((publisher) =>
@@ -142,20 +166,32 @@ const ManagementPublisher = () => {
   );
 
   return (
-    <div style={{ padding: "20px", backgroundColor: "#f5f5f5", borderRadius: "5px" }}>
+    <div
+      style={{
+        padding: "20px",
+        backgroundColor: "#f5f5f5",
+        borderRadius: "5px",
+      }}
+    >
       <ToastContainer />
-      <h1>Publishers</h1>
-      <div style={{ display: "flex", justifyContent: "space-between", marginBottom: "20px" }}>
+      <h1>Quản lý nhà xuất bản</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginBottom: "20px",
+        }}
+      >
         <Button
           variant="contained"
           color="primary"
           onClick={() => setOpenAdd(true)}
           startIcon={<Add />}
         >
-          Add new
+          Thêm mới
         </Button>
         <TextField
-          label="Search Publishers"
+          label="Tìm kiếm nhà xuất bản"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
           style={{ marginLeft: "20px", flex: 1 }}
@@ -163,7 +199,10 @@ const ManagementPublisher = () => {
       </div>
       <List style={{ backgroundColor: "#fff", borderRadius: "5px" }}>
         {filteredPublishers.map((publisher) => (
-          <ListItem key={publisher.publisherID} style={{ borderBottom: "1px solid #ccc" }}>
+          <ListItem
+            key={publisher.publisherID}
+            style={{ borderBottom: "1px solid #ccc" }}
+          >
             <ListItemText primary={publisher.publisherName} />
             <ListItemSecondaryAction>
               <IconButton
@@ -195,31 +234,31 @@ const ManagementPublisher = () => {
         ))}
       </List>
       <Dialog open={openAdd} onClose={() => setOpenAdd(false)}>
-        <DialogTitle>Add New Publisher</DialogTitle>
+        <DialogTitle>Thêm nhà xuất bản mới</DialogTitle>
         <DialogContent>
           <TextField
             fullWidth
-            label="Publisher Name"
+            label="Tên nhà xuất bản"
             value={newPublisher}
             onChange={(e) => setNewPublisher(e.target.value)}
           />
         </DialogContent>
         <DialogActions>
           <Button onClick={() => setOpenAdd(false)} color="primary">
-            Cancel
+            Hủy
           </Button>
           <Button onClick={createPublisher} color="primary">
-            Add
+            Thêm
           </Button>
         </DialogActions>
       </Dialog>
       {selectedPublisher && (
         <Dialog open={openEdit} onClose={() => setOpenEdit(false)}>
-          <DialogTitle>Edit Publisher</DialogTitle>
+          <DialogTitle>Chỉnh sửa nhà xuất bản</DialogTitle>
           <DialogContent>
             <TextField
               fullWidth
-              label="Publisher Name"
+              label="Tên nhà xuất bản"
               value={selectedPublisher.publisherName}
               onChange={(e) =>
                 setSelectedPublisher({
@@ -231,7 +270,7 @@ const ManagementPublisher = () => {
           </DialogContent>
           <DialogActions>
             <Button onClick={() => setOpenEdit(false)} color="primary">
-              Cancel
+              Hủy
             </Button>
             <Button
               onClick={() =>
@@ -242,7 +281,7 @@ const ManagementPublisher = () => {
               }
               color="primary"
             >
-              Update
+              Cập nhật
             </Button>
           </DialogActions>
         </Dialog>
